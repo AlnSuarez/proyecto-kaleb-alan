aa5e2396adb2365946d0d55f7ac9398eaf31f496
alnsuarez <alansuarezsan@gmail.com>
2024-04-30 23:44:12 -0600

    Se agregaron los colores para dark mode y white mode

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -5,19 +5,27 @@ import { Button } from '@mui/material';
 import Counter from './Counter';
 
 function Pomodoro() {
-	const [play, setPlay] = useState(false);
-
-	return (
-		<>
-			<div>Pomodoro</div>
-			<Timer play={play} setPlay={setPlay}/>
-			<Counter />
-			<Button value={play} onClick={() => setPlay(!play)}>
-				{play ? 'STOP' : 'START'}
-			</Button>
-			<ModalConfig />
-		</>
-	);
+    const [play, setPlay] = useState(false);
+    
+    return (
+        <>
+            <div className='bg-light-bg-100 dark:bg-dark-bg-100'>
+                <h1>Pomodoro</h1>
+                <Timer
+                    play={play}
+                    setPlay={setPlay}
+                />
+                <Counter />
+                <Button
+                    value={play}
+                    onClick={() => setPlay(!play)}
+                >
+                    {play ? 'STOP' : 'START'}
+                </Button>
+                <ModalConfig />
+            </div>
+        </>
+    );
 }
 
 export default Pomodoro;
